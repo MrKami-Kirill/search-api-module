4e9f562d0b8bfab09fad541567afcf4b0b8e0bd2
kgaleev <kgaleev@rtech.ru>
2026-02-20 16:29:16 +0300

    init commit 30

---
+++ search-models/src/main/java/ru/tecius/telemed/dto/request/PathWithValue.java
@@ -0,0 +1,11 @@
+package ru.tecius.telemed.dto.request;
+
+import jakarta.persistence.criteria.Path;
+import java.util.List;
+
+/**
+ * Record to hold Path, transformed values, and field type for Criteria API predicate building.
+ * Used as a parameter for criteriaPredicateFunction in Operator enum.
+ */
+public record PathWithValue(Path<?> path, List<String> values, Class<?> fieldType) {
+}
